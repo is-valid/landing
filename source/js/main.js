ce617be73598e01860ada94215df832e2ac46379
Yaroslav Omelianenko <Omelianenkoyarosllav@.com>
2020-06-15 18:15:50 +0300

    js for open form was added

---
+++ source/js/main.js
@@ -0,0 +1,11 @@
+﻿(function () {
+
+    var openFormButton = document.querySelector('.arrow-down');
+
+    if (openFormButton) {
+        openFormButton.addEventListener('click', function (e) {
+            e.preventDefault();
+            form.open();
+        })
+    }
+}());
