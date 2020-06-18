c65ab652b7770be9ba3b9ee0ee400092173c458e
Yaroslav Omelianenko <Omelianenkoyarosllav@.com>
2020-06-18 11:54:33 +0300

    validation form was added

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,38 @@
 ﻿(function () {
-
     var openFormButton = document.querySelector('.arrow-down');
+    var form = document.querySelector('.form');
+    var nav = document.querySelector('.nav');
 
     if (openFormButton) {
         openFormButton.addEventListener('click', function (e) {
             e.preventDefault();
-            form.open();
+            Line.form.open();
         })
     }
+
+    if (form) {
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+            if (Line.form.isValid()) {
+                console.log('All good');
+            } else {
+                console.log('Is not valid');
+            }
+
+        })
+    }
+
+    if (nav) {
+        nav.addEventListener('click', function (e) {
+            var target = e.target;
+
+            if (target.tagName.toLowerCase() !== 'a') {
+                return;
+            }
+
+            e.preventDefault();
+            Line.navigation.toggleToActiveLink(target);
+        });
+    }
+
 }());
